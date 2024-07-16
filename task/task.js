5d8d1234bb555956494ca8cca6eb8f87caca3d9c
Nill <niloufar.shahdoust@utah.edu>
2024-07-16 16:24:19 -0600

    some updates added

--- task/task.js
+++ task/task.js
@@ -1,3 +1,9 @@
+// If using modules
+var timeline;
+if (typeof require !== 'undefined') {
+  timeline = require('./task_description.js');
+}
+
 var jsPsych = initJsPsych({
   experiment_width: 1000,
   on_finish: function() {
@@ -6,68 +12,6 @@ var jsPsych = initJsPsych({
   },
   override_safe_mode: true
 });
-
-var timeline = [];
-
-var title_screen = {
-  type: jsPsychHtmlButtonResponse,
-  stimulus: `
-    <div class="center">
-      <div class="logo-title">
-        <img src="img/logo.png" width="20%">
-        <h1>Balloon Analog Risk Task (BART)</h1>
-      </div>
-    </div>
-  `,
-  choices: ['Start']
-};
-timeline.push(title_screen);
-
-var confs = {
-  type: jsPsychSurveyMultiChoice,
-  questions: [
-    {
-      prompt: "This study works properly only on PCs and laptops, and not on smartphones or tablets. Before you proceed, please confirm that you take part via Desktop PC or Laptop.",
-      name: 'DesktopConf',
-      options: ['1: I confirm', '2: I do not confirm'],
-      required: true
-    }
-  ],
-  preamble: `
-    <p><img src="img/logo.png" width="20%"></p>
-    <p><b>Welcome to this experiment and thank you very much for your participation.</b></p>
-  `,
-  on_finish: function(data) {
-    var responses = data.response;
-    if (responses.DesktopConf === '2: I do not confirm') {
-      jsPsych.endExperiment('The experiment has been terminated because you are not using a Desktop PC or Laptop.');
-    }
-  }
-};
-
-timeline.push(confs);
-
-var task_description = {
-  type: jsPsychHtmlButtonResponse,
-  stimulus: `
-    <div class="center">
-      <h2>Task description</h2>
-      <img src="img/task.png" width="50%">
-      <p>The task begins with the initial screen showing a small balloon (yellow/orange/red/grey).</p>
-      <p><b>Inflate Start:</b> The balloon starts to inflate once you press inflate button.</p>
-      <p><b>Inflation Phase:</b> During the inflation phase, the balloon grows larger, and you decide when to stop inflating it.</p>
-      <p><b>Trial End:</b> The trial ends when you either stop inflating the balloon or the balloon pops.</p>
-      <p><b>Outcome:</b></p>
-      <ul><li>If you stop inflating the balloon before it pops, you <b>bank</b> the money accumulated and the trial ends successfully.</li>
-        <li>If the balloon size exceeds a certain maximum threshold, the balloon <b>pops</b>, and you lose the money for that trial.</li>
-      </ul>
-      <p><b>Note:</b></p>
-      <p>A grey balloon is a passive trial, you can just watch it.</p>
-    </div>
-  `,
-  choices: ['Next']
-};
-timeline.push(task_description);
 
 var balloonColors = ["red", "orange", "yellow"];
 var colorMeans = {
@@ -107,7 +51,7 @@ for (let i = 0; i < 10; i++) {
           </div>
           <div class="button-container">
             <button id="inflate" class="inflate-button">inflate</button>
-            <button id="bank" class="bank-button">bank</button>
+            <button id="bank" class="bank-button" style="display: none;">bank</button>
           </div>
         </div>
       `;
@@ -121,37 +65,40 @@ for (let i = 0; i < 10; i++) {
       let totalRewardElement = document.querySelector('.total-reward');
       let balloonSize = 50;
       let reward = 0;
+      let inflationInterval;
 
       inflateButton.addEventListener('click', function() {
-        inflateButton.classList.add('active');
-        setTimeout(() => {
-          inflateButton.classList.remove('active');
-        }, 100); // Remove class after 100ms for visual effect
+        inflateButton.style.display = 'none'; // Hide the inflate button
+        bankButton.style.display = 'block'; // Show the bank button
 
-        if (balloonSize < maxBalloonSize) {
-          balloonSize += 20;
-          reward += 1; // Increment the reward by 1 with each inflate
-          balloon.style.width = `${balloonSize}px`;
-          balloon.style.height = `${balloonSize}px`;
-          rewardElement.textContent = reward; // Update the reward display
-        } else {
-          balloon.style.display = 'none';
-          inflateButton.style.display = 'none';
-          bankButton.style.display = 'none';
-          let trialContainer = document.querySelector('.trial-container');
-          let popMessage = document.createElement('div');
-          popMessage.innerHTML = 'popped!';
-          popMessage.style.fontSize = '50px';
-          popMessage.style.color = 'red';
-          popMessage.style.fontWeight = 'bold';
-          trialContainer.appendChild(popMessage);
-          setTimeout(jsPsych.finishTrial, 1000);
-        }
+        inflationInterval = setInterval(function() {
+          if (balloonSize < maxBalloonSize) {
+            balloonSize += 20;
+            reward += 1; // Increment the reward by 1 with each inflate
+            balloon.style.width = `${balloonSize}px`;
+            balloon.style.height = `${balloonSize}px`;
+            rewardElement.textContent = reward; // Update the reward display
+          } else {
+            clearInterval(inflationInterval); // Stop the inflation
+            balloon.style.display = 'none';
+            inflateButton.style.display = 'none';
+            bankButton.style.display = 'none';
+            let trialContainer = document.querySelector('.trial-container');
+            let popMessage = document.createElement('div');
+            popMessage.innerHTML = 'popped!';
+            popMessage.style.fontSize = '50px';
+            popMessage.style.color = 'red';
+            popMessage.style.fontWeight = 'bold';
+            trialContainer.appendChild(popMessage);
+            setTimeout(jsPsych.finishTrial, 1000);
+          }
+        }, 100); // Inflate every 100 ms
       });
 
       bankButton.addEventListener('click', function() {
+        clearInterval(inflationInterval); // Stop the inflation
         totalReward += reward; // Add the current reward to the total reward
-        totalRewardElement.textContent = `total reward:  ${totalReward}`; // Update the total reward display
+        totalRewardElement.textContent = `total reward: $ ${totalReward}`; // Update the total reward display
         reward = 0; // Reset the current reward
         rewardElement.textContent = reward; // Update the reward display
         balloon.style.display = 'none'; // Hide the balloon
---
+++ task/task_description.js
@@ -0,0 +1,94 @@
+var timeline = [];
+
+var title_screen = {
+  type: jsPsychHtmlButtonResponse,
+  stimulus: `
+    <div class="center">
+      <div class="logo-title">
+        <img src="img/logo.png" width="20%">
+        <h1>Balloon Analog Risk Task (BART)</h1>
+      </div>
+    </div>
+  `,
+  choices: ['Start']
+};
+timeline.push(title_screen);
+
+var confs = {
+  type: jsPsychSurveyMultiChoice,
+  questions: [
+    {
+      prompt: "This study works properly only on PCs and laptops, and not on smartphones or tablets. Before you proceed, please confirm that you take part via Desktop PC or Laptop.",
+      name: 'DesktopConf',
+      options: ['1: I confirm', '2: I do not confirm'],
+      required: true
+    }
+  ],
+  preamble: `
+    <p><img src="img/logo.png" width="20%"></p>
+    <p><b>Welcome to this experiment and thank you very much for your participation.</b></p>
+  `,
+  on_finish: function(data) {
+    var responses = data.response;
+    if (responses.DesktopConf === '2: I do not confirm') {
+      jsPsych.endExperiment('The experiment has been terminated because you are not using a Desktop PC or Laptop.');
+      setTimeout(() => {
+        window.location.reload(); // Reloads the page after showing the message
+      }, 3000); // Waits for 3 seconds before reloading
+    }
+  }
+};
+
+
+timeline.push(confs);
+
+
+
+
+var consent_form = {
+  type: jsPsychSurveyMultiChoice,
+  questions: [
+    {
+      prompt: "Do you agree?",
+      name: 'DesktopConf',
+      options: ['1: I agree.'],
+      required: true
+    }
+  ],
+  preamble: `
+<p><img src="img/logo.png" width="20%"></p>
+  <p><b>Consent form.</b></p>
+`
+};
+
+timeline.push(consent_form);
+
+
+
+var task_description = {
+  type: jsPsychHtmlButtonResponse,
+  stimulus: `
+    <div class="center">
+      <h2>Task description</h2>
+      <img src="img/task.png" width="50%">
+      <p>The task begins with the initial screen showing a small balloon (yellow/orange/red/grey).</p>
+      <p><b>Inflate Start:</b> The balloon starts to inflate once you press inflate button.</p>
+      <p><b>Inflation Phase:</b> During the inflation phase, the balloon grows larger, and you decide when to stop inflating it.</p>
+      <p><b>Trial End:</b> The trial ends when you either stop inflating the balloon or the balloon pops.</p>
+      <p><b>Outcome:</b></p>
+      <ul><li>If you stop inflating the balloon before it pops, you <b>bank</b> the money accumulated and the trial ends successfully.</li>
+        <li>If the balloon size exceeds a certain maximum threshold, the balloon <b>pops</b>, and you lose the money for that trial.</li>
+      </ul>
+      <p><b>Note:</b></p>
+      <p>A grey balloon is a passive trial, you can just watch it.</p>
+    </div>
+  `,
+  choices: ['Next']
+};
+
+timeline.push(task_description);
+
+// Export timeline if using modules
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = timeline;
+}
